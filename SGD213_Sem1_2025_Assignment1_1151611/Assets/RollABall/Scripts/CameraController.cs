b6a3873d984a2925ae686e932ede9f16edd2a6f6
abp015 <abp015@student.usc.edu.au>
2025-03-12 11:41:10 +1000

    Update Code

--- SGD213_Sem1_2025_Assignment1_1151611/Assets/RollABall/Scripts/CameraController.cs
+++ SGD213_Sem1_2025_Assignment1_1151611/Assets/RollABall/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	// store a public reference to the Player game object, so we can refer to it's Transform
+	[SerializeField]
 	public GameObject player;
 	// Store a Vector3 offset from the player (a distance to place the camera from the player at all times)
 	private Vector3 offset;
@@ -13,7 +14,15 @@ public class CameraController : MonoBehaviour {
 		// Create an offset by subtracting the Camera's position from the player's position
 		offset = transform.position - player.transform.position;
 	}
+
     void Update()
-	{ }
+	{ 
+	}
+
     // After the standard 'Update()' loop runs, and just before each frame is rendered..
-    void LateUpdate (){transform.position = player.transform.position + offset;}}
+    void LateUpdate ()
+	{
+		// Repeat the start calculation at the end of every frame to follow player
+		transform.position = player.transform.position + offset;
+	}
+}
--- SGD213_Sem1_2025_Assignment1_1151611/Assets/RollABall/Scripts/PlayerController.cs
+++ SGD213_Sem1_2025_Assignment1_1151611/Assets/RollABall/Scripts/PlayerController.cs
@@ -2,52 +2,64 @@
 
 // Include the namespace required to use Unity UI
 using UnityEngine.UI;
-
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     // Create public variables for player speed, and for the Text UI game objects
     [SerializeField]
-    public float f_horPlayAccel;
+    private float horizontalAcceleration;
 
-	public Text NumTotal;
-	public Text wintext;
+	private Text scoreText;
+	private Text winText;
 
 	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
 	private Rigidbody rb;
-	private float inttotalcount;
+	private float score;
 
 	// At the start of the game..
-	void Start ()	{rb = GetComponent<Rigidbody>();inttotalcount = 0;SetCountText ();wintext.text = "";}
+	void Start ()	
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(200, 0, 200);
+		score = 0;
+		SetCountText();
+		winText.text = "";
+	}
 
 	void FixedUpdate ()
 	{
 		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+
 		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce (movement * f_horPlayAccel);
-	}
-	void OnTriggerEnter(Collider other) 
-{
-if (other.gameObject.CompareTag ("Pick Up"))
-{							other.gameObject.SetActive (false);
-							inttotalcount = inttotalcount + 1;
-							SetCountText ();
-		}}
 
-    void DoSomething()
-	{
-	
+		rb.AddForce (movement * horizontalAcceleration);
 	}
+
+	// Function to count pickup score
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+
+		{
+			other.gameObject.SetActive (false);
+			score = score + 1;
+			SetCountText ();
+		}
+	}
+
+	// Function to update UI text
     void SetCountText()
-{NumTotal.text = "Count: " + inttotalcount.ToString ();
-		if (inttotalcount >= 12) 
-{
+	{
+		if (score == 0) return;
+		scoreText.text = "Count: " + score.ToString ();
+		if (score >= 12) 
+		{
 			// Set the text value of our 'winText'
-			wintext.text = "You Win!";
-}
-}
+			winText.text = "You Win!";
+		}
+	}
 }
